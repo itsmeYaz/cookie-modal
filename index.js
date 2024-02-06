f779b6c8b3b07193a6317c6a4221abe0b85ad9c4
Nielven Mascariñas <mascarinas022@gmail.com>
2024-02-06 09:36:14 +0800

    Update modal button type and add loading and success messages

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ Challenge:
 const modal = document.getElementById("modal");
 const modalCloseBtn = document.querySelector("#modal-close-btn");
 const consentForm = document.querySelector("#consent-form");
+const modalText = document.querySelector("#modal-text");
 
 setTimeout(function () {
   modal.style.display = "block";
@@ -20,4 +21,27 @@ modalCloseBtn.addEventListener("click", () => {
 
 consentForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  modalText.innerHTML = `
+  <div class="modal-inner-loading">
+    <img src="images/loading.svg" class="loading">
+    <p id="uploadText">
+      Uploading your data to the dark web...
+    </p>
+  </div>
+  `;
+
+  setTimeout(() => {
+    document.querySelector("#uploadText").textContent = `Making the sale... `;
+  }, 1500);
+
+  setTimeout(() => {
+    document.querySelector("#modal-inner").innerHTML = `
+    <h2>Thanks you sucker! </h2>
+    <p>We just sold the rights to your eternal soul.</p>
+    <div class="idiot-gif">
+        <img src="images/pirate.gif">
+    </div>
+    `;
+  }, 3000);
 });
